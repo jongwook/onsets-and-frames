7a7b92913e411aedc3a4e0a9cf2c5bdbe556b177
Jong Wook Kim <jongwook@nyu.edu>
2018-12-05 16:36:34 -0500

    added requirements.txt and prepare_maestro.sh

---
+++ data/prepare_maestro.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+set -e
+
+echo Downloading the MAESTRO dataset \(87 GB\) ...
+curl -O https://storage.googleapis.com/magentadata/datasets/maestro/v1.0.0/maestro-v1.0.0.zip
+
+echo Extracting the files ...
+unzip -o maestro-v1.0.0.zip | awk 'BEGIN{ORS=""} {print "\rExtracting " NR "/2381 ..."; system("")} END {print "\ndone"}'
+
+rm maestro-v1.0.0.zip
+mv maestro-v1.0.0 MAESTRO
+
+echo Converting the audio files to FLAC ...
+COUNTER=0
+for f in MAESTRO/*/*.wav; do
+    COUNTER=$((COUNTER + 1))
+    echo -ne "\rConverting ($COUNTER/1184) ..."
+    ffmpeg -y -loglevel fatal -i $f -ac 1 -ar 16000 ${f/\.wav/.flac}
+done
+
+echo 
+echo Preparation complete!
