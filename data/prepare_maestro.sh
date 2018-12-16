46a5cc8dc50d62e9a986d0bbfc574a1bf009ca35
Jong Wook Kim <jongwook@nyu.edu>
2018-12-16 05:13:33 -0500

    update prepare_maestro.sh

--- data/prepare_maestro.sh
+++ data/prepare_maestro.sh
@@ -5,7 +5,7 @@ echo Downloading the MAESTRO dataset \(87 GB\) ...
 curl -O https://storage.googleapis.com/magentadata/datasets/maestro/v1.0.0/maestro-v1.0.0.zip
 
 echo Extracting the files ...
-unzip -o maestro-v1.0.0.zip | awk 'BEGIN{ORS=""} {print "\rExtracting " NR "/2381 ..."; system("")} END {print "\ndone"}'
+unzip -o maestro-v1.0.0.zip | awk 'BEGIN{ORS=""} {print "\rExtracting " NR "/2383 ..."; system("")} END {print "\ndone\n"}'
 
 rm maestro-v1.0.0.zip
 mv maestro-v1.0.0 MAESTRO
@@ -18,5 +18,5 @@ for f in MAESTRO/*/*.wav; do
     ffmpeg -y -loglevel fatal -i $f -ac 1 -ar 16000 ${f/\.wav/.flac}
 done
 
-echo 
+echo
 echo Preparation complete!
